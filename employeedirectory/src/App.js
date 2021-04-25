e7a78b1d490d4f81de837f47f0bd37d5cc6ddd60
AP <past0073@umn.edu>
2021-04-25 09:44:58 -0500

    Added search bar.

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import Search from "./components/Search";
 
 function App() {
     return (
         <div>
         <Header />
+        <Search />
        
         </div>
     );
---
+++ employeedirectory/src/components/Search/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./style.css";
+
+function Search(props) {
+    return (
+        <form className="form-inline my-2 my-lg-0">
+        <input 
+        value={props.search}
+        onChange={props.handleInputChange}
+        className="form-control mr-sm-2" 
+        type="search" 
+        placeholder="Search" 
+        aria-label="Search"
+        />
+        <button onClick={props.handleFormSubmit} className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    );
+};
+  
+
+export default Search;
